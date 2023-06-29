b445f9ab116c1982362e1350186a4454dafff392
NewHorizon2023 <lxnirl@gmail.com>
2023-06-29 16:23:21 +0100

    change the path of unit test class

--- src/DataStructure/UnitTest.java
+++ src/DataStructure/UnitTest.java
@@ -1,13 +1,13 @@
-package DataStructure.LinkedList.SampleProblems;
+package DataStructure;
 
+import DataStructure.LinkedList.SampleProblems.LinedList;
 import org.junit.jupiter.api.Test;
 
 /**
  * @author 刘祥楠
- * @date 2023-06-29 15:53
+ * @date 2023-05-30 17:05
  */
-public class SampleProblemsTest {
-
+public class UnitTest {
     @Test
     public void sizeTest() {
         LinedList list = new LinedList();
@@ -17,5 +17,4 @@ public class SampleProblemsTest {
         System.out.println(list.size());
         System.out.println(list.getAverage());
     }
-
 }
