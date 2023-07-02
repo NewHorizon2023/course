3a5ea8cafd91a802e06bac9346dfeedbca28ca71
xiangnan.liu <x22190562@student.ncirl.ie>
2023-07-02 07:21:22 +0100

    optimize merge sort

--- src/Algorithms/Sort/MergeSort.java
+++ src/Algorithms/Sort/MergeSort.java
@@ -1,6 +1,8 @@
 package Algorithms.Sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MergeSort {
     public static int[] mergeSort(int[] arr) {
@@ -43,7 +45,7 @@ public class MergeSort {
             }
         }
 
-        for (i = i; i < newArr.length; i++) {
+        for (; i < newArr.length; i++) {
             newArr[i] = arrSign[indexSign];
             indexSign++;
         }
@@ -51,16 +53,45 @@ public class MergeSort {
         return newArr;
     }
 
+    public static int[] mergeSort2(int[] arr) {
+        List<int[]> list = new ArrayList<>(arr.length);
+        for (int j : arr) {
+            list.add(new int[]{j});
+        }
+
+        while (list.size() > 1) {
+            List<int[]> subList = new ArrayList<>(list.size());
+            for (int i = 0; i < list.size(); i = i + 2) {
+                if (i + 1 == list.size()) {
+                    subList.add(list.get(list.size() - 1));
+                    break;
+                }
+                subList.add(merge(list.get(i), list.get(i + 1)));
+            }
+            list = subList;
+        }
+
+        return list.get(0);
+    }
+
     public static void main(String[] args) {
-        int[] arr = new int[10000];
+        int[] arr = new int[1000000];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (arr.length * Math.random());
         }
+        int[] arr1 = Arrays.copyOf(arr, arr.length);
+
         long start = System.currentTimeMillis();
-        mergeSort(arr);
+        arr = mergeSort(arr);
         long end = System.currentTimeMillis();
         System.out.println("Insertion sort time: " + (end - start) + "ms");
-        System.out.println(Arrays.toString(arr));
+        //System.out.println(Arrays.toString(arr));
+
+        long start1 = System.currentTimeMillis();
+        arr1 = mergeSort2(arr1);
+        long end1 = System.currentTimeMillis();
+        System.out.println("Insertion sort2 time: " + (end1 - start1) + "ms");
+        //System.out.println(Arrays.toString(arr1));
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int i = -123;
         String str = String.valueOf(i);
         String k = "0123-";
         System.out.println(Integer.parseInt(k));
+
+        List list = new ArrayList<>();
+        list.add(123);
+        list.add("abc");
+        System.out.println(list);
     }
 }
